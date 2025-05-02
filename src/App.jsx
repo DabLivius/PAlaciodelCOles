1a187dfcab72dd0a230a921152ff3d4dfdd50f2f
Amaydys <amaydys2002>
2025-05-02 12:49:30 -0500

    actualizacion de la tienda.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Tienda from './componentes/Tienda';
-
+import Empanadas from './pages/Empanadas';
+import Hamburguesa from './pages/Hamburguesa';
+import Papas from './pages/Papas';
+import Pasteles from './pages/Pasteles';
+import Caliente from './pages/Caliente';
+import Picada  from './pages/Picada';
+import Pollo  from './pages/Pollo';
+import Relleno  from './pages/Relleno';
 function App() {
   return (
-    <div>
-      <Tienda />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Tienda />} />
+        <Route path="/empanadas" element={<Empanadas />} />
+        <Route path="/hamburguesa" element={<Hamburguesa />} />
+        <Route path="/papas" element={<Papas />} />
+        <Route path="/pasteles" element={<Pasteles />} />
+        <Route path="/caliente" element={<Caliente />} />
+        <Route path="/picada" element={<Picada />} />
+        <Route path="/pollo" element={<Pollo />} />
+        <Route path="/relleno" element={<Relleno />} />
+        <Route path="/tienda" element={<Tienda />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/componentes/Tienda.jsx
+++ src/componentes/Tienda.jsx
@@ -1,11 +1,13 @@
 // src/componentes/Tienda.jsx
-import '../estilos/Tienda.css';
-import empanadaImg from '../img/empandacarne.jpg';
+import { Link } from 'react-router-dom'
+import '../estilos/Tienda.css'
+import coldImg from '../img/cold.jpg'
+import empanadaImg from '../img/empandacarne.jpg'
 import rellenoImg from '../img/relleno.jpg'
 import pastelImg from '../img/pastelfrito.jpg'
 import hamburguesaImg from '../img/hamburguesa.jpg'
 import papaImg from '../img/papasrellenas.jpg'
-import perroImg from '../img/Perro-Caliente.jpg'
+import CalienteImg from '../img/Caliente.jpg'
 import polloImg from '../img/pollo.jpg'
 import picadaImg from '../img/MG_1606.jpg.webp'
 
@@ -13,12 +15,11 @@ import picadaImg from '../img/MG_1606.jpg.webp'
 export default function Tienda() {
   return (
     <>
-      <header>
-        Paco <img src="cold.jpg" height="50" width="50" alt="Logo" />
+      <header>  
+        Paco <img src={coldImg} height="50" width="50" alt="Logo" />
         <nav>
           <ul>
-            <li><a href="tienda.html">Tienda</a></li>
-            <li><a href="#">Carrito</a></li>
+            <li><Link to="/Tienda">Tienda</Link></li>
             <li><a href="#">Iniciar sesión</a></li>
           </ul>
         </nav>
@@ -33,56 +34,56 @@ export default function Tienda() {
           <img src={empanadaImg} alt="Empanadas de carne" />
           <h3>Empanadas</h3>
           <p className="price">$3.000</p>
-          <a href="empanadas.html"><button className="buy-button">Comprar</button></a>
+          <Link to="/Empanadas"><button className="buy-button">Comprar</button></Link>
         </div>
 
         <div className="item">
           <img src={pastelImg} alt="Pasteles fritos" />
           <h3>Pasteles fritos</h3>
           <p className="price">$3.000</p>
-          <a href="pasteles.html"><button className="buy-button">Comprar</button></a>
+          <Link to="/Pasteles"><button className="buy-button">Comprar</button></Link>
         </div>
 
         <div className="item">
           <img src={hamburguesaImg} alt="Hamburguesas" />
           <h3>Hamburguesas</h3>
           <p className="price">$12.000</p>
-          <a href="hamburguesa.html"><button className="buy-button">Comprar</button></a>
+          <Link to="/Hamburguesa"><button className="buy-button">Comprar</button></Link>
         </div>
 
         <div className="item">
           <img src={papaImg} alt="Papas rellenas" />
           <h3>Papas rellenas</h3>
           <p className="price">$3.000</p>
-          <a href="papas.html"><button className="buy-button">Comprar</button></a>
+          <Link to="/Papas"><button className="buy-button">Comprar</button></Link>
         </div>
 
         <div className="item">
-          <img src={perroImg} alt="Perro-Caliente" />
+          <img src={CalienteImg} alt="Perro-Caliente" />
           <h3>Perro Caliente</h3>
           <p className="price">$14.000</p>
-          <a href="perrocaliente.html"><button className="buy-button">Comprar</button></a>
+          <Link to="/Caliente"><button className="buy-button">Comprar</button></Link>
         </div>
 
         <div className="item">
           < img src={polloImg} alt="Pollo a la Broster" />
           <h3>Pollo a la Broster</h3>
           <p className="price">$10.000</p>
-          <a href="pollo.html"><button className="buy-button">Comprar</button></a>
+          <Link to="/pollo"><button className="buy-button">Comprar</button></Link>
         </div>
 
         <div className="item">
           <img src={picadaImg} alt="Picada del palacio" />
           <h3>Picada del palacio</h3>
           <p className="price">$25.000</p>
-          <a href="picadapalacio.html"><button className="buy-button">Comprar</button></a>
+          <Link to="/Picada"><button className="buy-button">Comprar</button></Link>
         </div>
 
         <div className="item">
           <img src={rellenoImg} alt="Relleno" />
           <h3>Relleno</h3>
           <p className="price">$16.000</p>
-          <a href="relleno.html"><button className="buy-button">Comprar</button></a>
+          <Link to="/Relleno"><button className="buy-button">Comprar</button></Link>
         </div>
       </section>
     </>
